e9d914f649d468b84fdfea84607ec08728a76268
aymen benso <117603473+aymen-benso@users.noreply.github.com>
2024-04-17 09:45:32 +0000

    done

--- my-app/app/admin/Planning/page.tsx
+++ my-app/app/admin/Planning/page.tsx
@@ -3,16 +3,56 @@
 import * as React from "react"
 
 import { Calendar } from "@/components/ui/calendar"
+import { CalendarDays } from "lucide-react"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
 
 export default function CalendarDemo() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
 
   return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="rounded-md border"
-    />
+    <div>
+                <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                className="rounded-md border"
+                />
+                <HoverCard>
+                <HoverCardTrigger asChild>
+                <Button variant="link">Cywork Event</Button>
+                </HoverCardTrigger>
+                <HoverCardContent className="w-80">
+                <div className="flex justify-between space-x-4">
+                    <Avatar>
+                    <AvatarImage src="https://github.com/vercel.png" />
+                    <AvatarFallback>VC</AvatarFallback>
+                    </Avatar>
+                    <div className="space-y-1">
+                    <h4 className="text-sm font-semibold">Cywork Event</h4>
+                    <p className="text-sm">
+                        Cywork Event For Students and Developers
+                    </p>
+                    <div className="flex items-center pt-2">
+                        <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                        <span className="text-xs text-muted-foreground">
+                        Participation April 2024
+                        </span>
+                    </div>
+                    </div>
+                </div>
+                </HoverCardContent>
+            </HoverCard>
+  </div>
   )
 }
